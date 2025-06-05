28d1437e6a83dc3a1b9352ef189e8adc75faee00
Aayush Mishra <manofganges@gmail.com>
2025-06-05 18:44:58 +0530

    Update code-1.asm

--- assembly_language/code-1.asm
+++ assembly_language/code-1.asm
@@ -1,11 +1,11 @@
-AREA main, CODE, READONLY
-EXPORT __main ; make main visible to linker
-ENTRY
+  AREA main, CODE, READONLY
+  EXPORT __main ; make main visible to linker
+  ENTRY
 __main
-MOV R0, #0
-MOV R1, #4
-MOV R2, #6
-ADD R3, R2, R1
-ALIGN
-AREA allocations, DATA, READWRITE
-END
+  MOV R0, #0
+  MOV R1, #4
+  MOV R2, #6
+  ADD R3, R2, R1
+  ALIGN
+  AREA allocations, DATA, READWRITE
+  END
